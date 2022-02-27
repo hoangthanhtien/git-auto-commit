a386a49292cc42bd002b0ad928bcb07474752145
tienhoang <tienhoang@pop-os.localdomain>
2022-02-27 16:03:48 +0700

    [In Progress] Add file sh script to auto commit in git

--- auto_git_commit.sh
+++ auto_git_commit.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 GIT_PATH=$1
+PUSH_MASTER_BRANCH=$2
 cd $GIT_PATH
 LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B | cat)
 CURRENT_GIT_BRANCH=$(git branch --show-current)
-if [ $CURRENT_GIT_BRANCH = "master" ] || [ $CURRENT_GIT_BRANCH = "main" ]; then
+if ([ $CURRENT_GIT_BRANCH = "master" ] || [ $CURRENT_GIT_BRANCH = "main" ]) && [ "$PUSH_MASTER_BRANCH" != "--push-master" ]; then
   echo  "ERROR: Auto commit on branch ${CURRENT_GIT_BRANCH} is disabled"
 else
-  AUTO_COMMIT_MESSAGE="[In Progress] ${LAST_COMMIT_MESSAGE}"
-  echo 'LAST_COMMIT_MESSAGE' $LAST_COMMIT_MESSAGE
-  echo 'AUTO_COMMIT_MESSAGE' $AUTO_COMMIT_MESSAGE
+  if [[ $LAST_COMMIT_MESSAGE = "[In Progress]"* ]]; then
+    AUTO_COMMIT_MESSAGE=$LAST_COMMIT_MESSAGE
+  else
+    AUTO_COMMIT_MESSAGE="[In Progress] ${LAST_COMMIT_MESSAGE}"
+  fi
+  git add .
+  git commit -m "${AUTO_COMMIT_MESSAGE}"
+  git push --set-upstream origin $CURRENT_GIT_BRANCH
 fi
-#cd $GIT_PATH && git commit
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 from subprocess import call, STDOUT
 import os
+from crontab import CronTab
 from os import path
 
 # Get the git folder path
@@ -12,7 +13,7 @@ if not path.exists(git_folder_path):
     raise Exception(f"{git_folder_path} is not exists, please check")
 
 # Check if input path is a git folder by checking if a .git folder exists
-if not path.exists(git_folder_path+"/.git"):
+if not path.exists(git_folder_path + "/.git"):
     raise Exception(f"{git_folder_path} is not a git repository")
 
 # Get the commit time everyday
@@ -35,6 +36,24 @@ try:
         raise Exception("Minute is not in the valid format")
 except Exception:
     raise Exception("You must enter the valid commit time")
-print("hour, minute", hour, minute)
-print("###########################################")
+
+
+# Enable push to master or main branch
+def yes_or_no(question):
+    reply = str(input(question + ' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return yes_or_no("Uhhhh... please enter ")
+
+
+enable_push_master = yes_or_no("Enable push to master or main branch?")
+
+# Setup conjob for auto commit
+cron = CronTab(user=True)
+job = cron.new(command=f'./auto_git_commit.sh {git_folder_path} {"--push-master" if enable_push_master else ""}')
+job.setall(int(minute), int(hour))
+cron.write()
 print(f"{git_folder_path} will be auto commit everyday at {commit_time}")
